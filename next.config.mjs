d1ccd943fd18b404f5ff1f2c662efd052177ad64
YsinHsim <yasinhassim43@gmail.com>
2024-12-16 14:44:16 +0800

    implement PWA for first time
    progressive web app feature for smartphone user.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/*
+
+/** @type {import('next').NextConfig} */
+
+/*const nextConfig = {};
+
+export default nextConfig;*/
+
+const withPWA = require('next-pwa')({
+    dest: 'public', // Specifies where the service worker and manifest are stored
+    register: true, // Registers the service worker automatically
+    skipWaiting: true, // Activates the new service worker without waiting
+});
+
+module.exports = withPWA({
+    reactStrictMode: true, // Other Next.js config
+});
